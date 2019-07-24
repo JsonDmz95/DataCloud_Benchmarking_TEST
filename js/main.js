0cd1799f53eb89e566f103cfa557fbdcf15fe6f2
Jeeson  Steven Lopez Dominguez <A0ATCBD00002@accedotech.net>
2019-07-24 15:45:16 -0700

    circle and dots animations added

--- js/main.js
+++ js/main.js
@@ -1,43 +1,83 @@
 var lFollowX = 0,
-    lFollowY = 0,
-    x = 0,
-    y = 0,
-    friction = 1 / 30;
+lFollowY = 0,
+x = 0,
+y = 0,
+friction = 1 / 30;
 
 function move() {
-  x += (lFollowX - x) * friction;
-  y += (lFollowY - y) * friction;
+    x += (lFollowX - x) * friction;
+    y += (lFollowY - y) * friction;
 
-  translate = 'translate(' + x + 'px, ' + y + 'px)';
+    translate = 'translate(' + x + 'px, ' + y + 'px)';
 
-  $('.phone').css({
-    '-webit-transform': translate,
-    '-moz-transform': translate,
-    'transform': translate
-  });
+    $('.phone').css({
+        '-webit-transform': translate,
+        '-moz-transform': translate,
+        'transform': translate,
+        'transition': 'none'
+    });
 
-  window.requestAnimationFrame(move);
+    window.requestAnimationFrame(move);
 }
 
 $(window).on('mousemove click', function(e) {
 
-  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-  lFollowX = (-7 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-  lFollowY = (-3.5 * lMouseY) / 100;
+    var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+    var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+    lFollowX = (-7 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+    lFollowY = (-3.5 * lMouseY) / 100;
 
 });
 
-move();
+
 
 
 
 function taglineEntrance() {
     $(".tagline").textillate({
-      in: { effect: "fadeInUp" }
+        in: { effect: "fadeInUp" }
     });
-  }
+}
 
-  $(document).ready(function(){
-      taglineEntrance();
-  });
+$(document).ready(function(){
+    taglineEntrance();
+
+    setTimeout(function() {
+        move();
+    }, 2000);
+
+
+
+    setTimeout(function() {
+        if($(window).width() >= 768){
+            $('#animated-circle').css({
+                'display': 'block'
+            });
+        }
+        new Vivus('animated-circle', {duration: 200});
+    }, 3500);
+
+    for (var i = 1; i <= 41; i++) {
+        // $(`#dot-${i}`).setAttribute('data-delay', `${4000 + (i * 200)}`);
+        // $('#dot-'+ i ).setAttribute('data-delay', 4000 + (i * 200));
+        // document.querySelector('#dot-1').setAttribute('data-delay', 4000 + (i * 200));
+        document.querySelector(`#dot-${i}`).dataset.delay = `${3500 + (i * 58)}`;
+    }
+
+});
+
+let act;
+function addCircle(){
+
+    if(window.innerWidth>=768 && act<768){
+        $('#animated-circle').css({
+            'display': 'block'
+        });
+    }
+
+    act = window.innerWidth;
+}
+
+window.addEventListener('resize', function(event){
+    addCircle();
+});
